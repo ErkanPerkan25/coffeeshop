56df6518d81257fbc731a3634830b7547f244ac5
Eric Hansson <erichansson.united@gmail.com>
2024-12-09 19:24:01 -0600

    Starting to finalize the website with meta tags, and finishing some of the content

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+function sendMessage(){
+    alert("Message have been sent! Thank you!");
+}
